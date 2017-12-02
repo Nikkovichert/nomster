da57ca3e2b80471a17ea333e447ba88c1c008e51
Nikko <nikkovichert@gmail.com>
2017-12-02 23:53:37 +0000

    geocoding

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -30,19 +30,19 @@ class PlacesController < ApplicationController
   end
 
   def update
-  @place = Place.find(params[:id])
+    @place = Place.find(params[:id])
 
-  if @place.user != current_user
-    return render text: 'Not Allowed', status: :forbidden
-  end
+    if @place.user != current_user
+      return render text: 'Not Allowed', status: :forbidden
+    end
 
-  @place.update_attributes(place_params)
-  if @place.valid?
-    redirect_to root_path
-  else
-    render :edit, status: :unprocessable_entity
+    @place.update_attributes(place_params)
+    if @place.valid?
+      redirect_to root_path
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
-end
 
   def destroy
   @place = Place.find(params[:id])
